6b322da21f87289a64b060d02b85386568bac16f
akshayaval <akshayaval36@gmail.com>
2026-08-23 23:17:39 +0530

    qs1

--- qs1.java
+++ qs1.java
@@ -1,23 +1,24 @@
 public class qs1
-{
-    static void checkPinLength(String pin)
-    {
-        if(pin.length() == 4)
-        {
-            System.out.println("PIN length OK");
+ {
+    static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean found = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    found = true;
+                }
+            }
         }
-        else
-        {
-            System.out.println("Invalid PIN");
-            System.out.println("PIN must be exactly 4 digits");
+
+        if (!found) {
+            System.out.println("No Duplicate Seats Found");
         }
     }
 
-    public static void main(String[] args)
-    {
-        String pin = "4820";
-
-        System.out.println("Checking PIN");
-        checkPinLength(pin);
+    public static void main(String[] args) {
+        int[] seats = {101, 102, 103, 102, 105};
+        checkDuplicateSeats(seats);
     }
 }
