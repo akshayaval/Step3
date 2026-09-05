4b5d34a35da61b7eb3d9eb427c3788de15f85e39
akshayaval <akshayaval36@gmail.com>
2026-09-05 14:52:59 +0530

    q4

--- qs4.java
+++ qs4.java
@@ -1,29 +1,46 @@
-import java.util.*;
+public class qs4 {
 
-class qs4 {
-    static String maskPhoneNumber(String phone) {
-        if (phone.length() != 10) {
-            return "Invalid phone number";
+    static double rowAverage(int[] row) {
+
+        int sum = 0;
+
+        for (int num : row) {
+            sum += num;
         }
 
-        for (int i = 0; i < phone.length(); i++) {
-            if (!Character.isDigit(phone.charAt(i))) {
-                return "Invalid phone number";
+        return (double) sum / row.length;
+    }
+
+    static String classifyRows(int[][] seatingScores, int threshold) {
+
+        String result = "";
+
+        for (int i = 0; i < seatingScores.length; i++) {
+
+            if (rowAverage(seatingScores[i]) >= threshold) {
+                result += "Row " + i + ": Buzzing Zone";
+            } else {
+                result += "Row " + i + ": Quiet Zone";
             }
+
+            if (i != seatingScores.length - 1) {
+                result += " | ";
+            }
+
         }
 
-        StringBuilder result = new StringBuilder("XXXXXX");
-        result.insert(6, "-");
-        result.append(phone.substring(6));
-
-        return result.toString();
+        return result;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String phone = sc.nextLine();
+        int[][] arr = {
+                {40,50,45},
+                {85,90,95},
+                {30,20,25}
+        };
 
-        System.out.println(maskPhoneNumber(phone));
+        System.out.println(classifyRows(arr,60));
+
     }
 }
