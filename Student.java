a7889d5b32d4daea8dae290304d122554bf1231e
akshayaval <akshayaval36@gmail.com>
2026-09-12 16:23:14 +0530

    q5

---
+++ Student.java
@@ -0,0 +1,25 @@
+class Student {
+    String name;
+    double attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println("College: " + collegeName);
+        System.out.println("Student Count: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Ravi", 85);
+        Student s2 = new Student("Priya", 90);
+
+        Student.printCollegeInfo();
+    }
+}
